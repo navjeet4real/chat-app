b330559ef9cb2b3f125a90b22c926beb67b89880
Navjeet Kajal <navjeetkajal.2594@gmail.com>
2023-01-05 18:10:25 +0530

    chat box

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,28 @@
+import { useTheme } from "@emotion/react";
+import { Stack, Box } from "@mui/system";
 import React from "react";
+import Conversation from "../../components/Conversation/Index";
 import Chats from "./Chats";
 
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
     <>
-      <Chats />
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        <Chats />
+        <Box
+          sx={{
+            height: "100%",
+            width: "calc(100vw - 420px)",
+            backgroundColor:
+              theme.palette.mode === "light"
+                ? "#fff"
+                : theme.palette.background.default,
+          }}
+        >
+          <Conversation />
+        </Box>
+      </Stack>
     </>
   );
 };
