93e66e9289b7096db8081f12011dae8c30bc309e
navjeet4real <navjeetkajal.2594@gmail.com>
2023-01-16 16:03:56 +0530

    login validations

--- src/sections/auth/LoginForm.js
+++ src/sections/auth/LoginForm.js
@@ -4,13 +4,22 @@ import FormProvider from "../../components/hook-form/FormProvider";
 import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Stack } from "@mui/system";
-import { Alert, InputAdornment } from "@mui/material";
+import {
+  Alert,
+  InputAdornment,
+  Stack,
+  IconButton,
+  Link,
+  Button,
+} from "@mui/material";
 import { RHFTextField } from "../../components/hook-form";
+import { Eye, EyeSlash } from "phosphor-react";
+import { bgcolor } from "@mui/system";
+import { useTheme } from "@emotion/react";
 
 const LoginForm = () => {
   const [showPassword, setShowPassword] = useState(false);
-
+  const theme = useTheme();
   const LoginSchema = Yup.object().shape({
     email: Yup.string()
       .required("Email is required")
@@ -23,7 +32,7 @@ const LoginForm = () => {
     password: "Damn@420",
   };
   const methods = useForm({
-    resolver: yupResolver(LoginForm),
+    resolver: yupResolver(LoginSchema),
     defaultValues,
   });
 
@@ -36,32 +45,65 @@ const LoginForm = () => {
 
   const onSubmit = async (data) => {
     try {
-        
     } catch (error) {
-        console.error(error)
-        reset();
-        setError("afterSubmit", {
-            ...error,
-            message: error.message
-        })
+      console.error(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
     }
-  }
+  };
   return (
-    <FormProvider
-      methods={methods}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-        <Stack spacing={3}>
-            {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
-        </Stack>
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
         <RHFTextField name="email" lable="Email address" />
-        <RHFTextField name="password" lable="Password" type={showPassword ? "text" : "password" } InputProps={{
-            endAdornment: {
-                // <InputAdornment />
-            }
-        }} />
-
-
+        <RHFTextField
+          name="password"
+          lable="Password"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
+                >
+                  {showPassword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <Stack alignItems={"flex-end"} sx={{ my: 2 }}>
+        <Link variant="body2" color="inherit" underline="always">
+          Forgot Password.?
+        </Link>
+      </Stack>
+      <Button
+        fullWidth
+        color="inherit"
+        size="large"
+        type="submit"
+        variant="contained"
+        sx={{
+          bgcolor: "text.primary",
+          color: (theme) =>
+            theme.palette.mode === "light" ? "common.white" : "grey",
+          "&hover": {
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey",
+          },
+        }}
+      >
+        Login
+      </Button>
     </FormProvider>
   );
 };
