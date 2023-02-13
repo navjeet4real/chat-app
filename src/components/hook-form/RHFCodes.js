8ad0b661c77aa076d14b18641e81ea9603a5239f
navjeet4real <navjeetkajal.2594@gmail.com>
2023-02-13 11:34:32 +0530

    removed bug from VerifyForm.js

--- src/components/hook-form/RHFCodes.js
+++ src/components/hook-form/RHFCodes.js
@@ -1,22 +1,23 @@
-import React from "react";
-// import PropTypes from 'prop-types';
-import { Stack, TextField } from "@mui/material";
-import { useRef } from "react";
-import { Controller , useFormContext } from "react-hook-form";
-import useEventListener from '../../hooks/useEventListner';
+import PropTypes from 'prop-types';
+import { useRef } from 'react';
+// form
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import { Stack, TextField } from '@mui/material';
+// hooks
+import useEventListener from '../../hooks/useEventListener';
 
-// RHFCodes.propTypes = {
-//     keyName: PropTypes.string,
-//     inputs: PropTypes.arrayOf(PropTypes.string),
-//   };
+// ----------------------------------------------------------------------
 
-export default function RHFCodes ({ keyName = "", inputs = [], ...other }) {
-  debugger
+RHFCodes.propTypes = {
+  keyName: PropTypes.string,
+  inputs: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default function RHFCodes({ keyName = '', inputs = [], ...other }) {
   const codesRef = useRef(null);
 
   const { control, setValue } = useFormContext();
-
-  console.log(inputs,"inputs")
 
   const handlePaste = (event) => {
     let data = event.clipboardData.getData('text');
@@ -29,61 +30,60 @@ export default function RHFCodes ({ keyName = "", inputs = [], ...other }) {
   };
 
   const handleChangeWithNextField = (event, handleChange) => {
-    const {maxLength, value, name} = event.target;
+    const { maxLength, value, name } = event.target;
 
-    const fieldIndex = name.replace(keyName, "")
+    const fieldIndex = name.replace(keyName, '');
+
     const fieldIntIndex = Number(fieldIndex);
 
-    const nextField = document.querySelector(`input[name=${keyName}${fieldIntIndex+1}]`)
+    const nextfield = document.querySelector(`input[name=${keyName}${fieldIntIndex + 1}]`);
 
-    if(value.length > maxLength){
-        event.target.value = value[0]
+    if (value.length > maxLength) {
+      event.target.value = value[0];
     }
-    if(value.length >=maxLength && fieldIntIndex < 6 && nextField !== null){
-        nextField.focus();
+
+    if (value.length >= maxLength && fieldIntIndex < 6 && nextfield !== null) {
+      nextfield.focus();
     }
+
     handleChange(event);
-  }
+  };
 
   useEventListener('paste', handlePaste, codesRef);
+
   return (
-    <>
-      <Stack direction="row" spacing={2} justifyContent="center" ref={codesRef}>
-        {inputs.map((name, index) => (
-          <Controller
-            key={name}
-            name={`${keyName}${index + 1}`}
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-              <TextField
-                {...field}
-                error={!!error}
-                autoFocus={index === 0}
-                placeholder="-"
-                onChange={(event) => {
-                  handleChangeWithNextField(event, field.onChange)
-                }}
-                onFocus={(event) => event.currentTarget.select()}
-                InputProps={{
-                    sx: {
-                        width: {xs: 36, sm: 56},
-                        height: {xs: 36, sm: 56},
-                        '& input': {p: 0, textAlign: 'center'}
-                    }
-                }}
-
-                inputProps={{
-                    maxLength: 1,
-                    type: 'number'
-                }}
-                {...other}
-              />
-            )}
-          />
-        ))}
-      </Stack>
-    </>
+    <Stack direction="row" spacing={2} justifyContent="center" ref={codesRef}>
+      {inputs.map((name, index) => (
+        <Controller
+          key={name}
+          name={`${keyName}${index + 1}`}
+          control={control}
+          render={({ field, fieldState: { error } }) => (
+            <TextField
+              {...field}
+              error={!!error}
+              autoFocus={index === 0}
+              placeholder="-"
+              onChange={(event) => {
+                handleChangeWithNextField(event, field.onChange);
+              }}
+              onFocus={(event) => event.currentTarget.select()}
+              InputProps={{
+                sx: {
+                  width: { xs: 36, sm: 56 },
+                  height: { xs: 36, sm: 56 },
+                  '& input': { p: 0, textAlign: 'center' },
+                },
+              }}
+              inputProps={{
+                maxLength: 1,
+                type: 'number',
+              }}
+              {...other}
+            />
+          )}
+        />
+      ))}
+    </Stack>
   );
-};
-
-// export default RHFCodes;
+}
--- src/sections/auth/VerifyForm.js
+++ src/sections/auth/VerifyForm.js
@@ -1,15 +1,17 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Stack } from "@mui/material";
 import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
+import FormProvider, { RHFTextField } from "../../components/hook-form";
 import RHFCodes from "../../components/hook-form/RHFCodes";
 import { VerifyEmail } from "../../redux/slices/auth";
+import { Eye, EyeSlash } from "phosphor-react";
 
 const VerifyForm = () => {
-    const dispatch = useDispatch();
-    const {email} = useSelector((state) => state.auth)
+  const dispatch = useDispatch();
+  const { email } = useSelector((state) => state.auth);
   const VerifyCodeSchema = Yup.object().shape({
     code1: Yup.string().required("Code is required"),
     code2: Yup.string().required("Code is required"),
@@ -34,48 +36,55 @@ const VerifyForm = () => {
     defaultValues,
   });
 
-  const { handleSubmit, formState } = methods;
+  const {
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = methods;
 
   const onSubmit = async (data) => {
     try {
-        dispatch(VerifyEmail({
-            email,
-            otp: `${data.code1}${data.code2}${data.code3}${data.code4}${data.code5}${data.code6}`
-        }))
+      dispatch(
+        VerifyEmail({
+          email,
+          otp: `${data.code1}${data.code2}${data.code3}${data.code4}${data.code5}${data.code6}`,
+        })
+      );
     } catch (error) {
       console.log(error);
     }
   };
-  return <>
-  <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-    <Stack spacing={3}>
+  return (
+    <>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          <RHFCodes
+            keyName="code"
+            inputs={["code1", "code2", "code3", "code4", "code5", "code6"]}
+          />
 
-    <RHFCodes 
-    keyName="code" 
-    inputs={["code1", "code2", "code3", "code4", "code5", "code6"]}/>
-
-    <Button
-        fullWidth
-        color="inherit"
-        size="large"
-        type="submit"
-        variant="contained"
-        sx={{
-          bgcolor: "text.primary",
-          color: (theme) =>
-            theme.palette.mode === "light" ? "common.white" : "grey",
-          "&hover": {
-            bgcolor: "text.primary",
-            color: (theme) =>
-              theme.palette.mode === "light" ? "common.white" : "grey",
-          },
-        }}
-      >
-        Verify
-      </Button> 
-    </Stack>
-  </FormProvider>
-  </>;
+          <Button
+            fullWidth
+            color="inherit"
+            size="large"
+            type="submit"
+            variant="contained"
+            sx={{
+              bgcolor: "text.primary",
+              color: (theme) =>
+                theme.palette.mode === "light" ? "common.white" : "grey",
+              "&hover": {
+                bgcolor: "text.primary",
+                color: (theme) =>
+                  theme.palette.mode === "light" ? "common.white" : "grey",
+              },
+            }}
+          >
+            Verify
+          </Button>
+        </Stack>
+      </FormProvider>
+    </>
+  );
 };
 
 export default VerifyForm;
